e796523eaa67cdfe9e7afca82d1ed7f6f0933a48
Irvan Aditya <iaditya9391@gmail.com>
2013-12-09 14:53:13 +0700

    dummy class

---
+++ src/java/sentiment/program/DummyClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sentiment.program;
+
+/**
+ *
+ * @author ize
+ */
+public class DummyClass {
+    
+}
